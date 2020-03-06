abe364165dd86a19a6a04af2058701566bdc2655
garrettfreshwater-nss <gfresh.nss@gmail.com>
2020-03-06 15:37:30 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace linq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Find the words in the collection that start with the letter 'L'
+            List<string> fruits = new List<string>() { "Lemon", "Apple", "Orange", "Lime", "Watermelon", "Loganberry" };
+
+            IEnumerable<string> LFruits = from fruit in fruits
+            where fruit.StartsWith("L")
+            select fruit;
+
+            foreach (var fruit in LFruits)
+            {
+                Console.WriteLine(fruit);
+            }
+
+            // Which of the following numbers are multiples of 4 or 6
+            var numbers = new List<int>()
+            {
+                15,
+                8,
+                21,
+                24,
+                32,
+                13,
+                30,
+                12,
+                7,
+                54,
+                48,
+                4,
+                49,
+                96
+            };
+
+            var fourSixMultiples = numbers.Where();
+        }
+    }
+}
